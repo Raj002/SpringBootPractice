5c7fc43f527de7e29db319550bc85b17dd3376e9
Rajkumar R <rajkumarr@Rajs-MacBook-Pro.local>
2023-04-05 20:03:39 +0530

    added exceptions

--- src/main/java/com/practice/rest/webservices/restwebservices/apis/PersonResource.java
+++ src/main/java/com/practice/rest/webservices/restwebservices/apis/PersonResource.java
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonResource {
 
-    @GetMapping(path = "/v1/person")
+    //@GetMapping(path = "/person", params = "version=1.0")
+    @GetMapping(path = "/person", headers = "X-API-VERSION=1.0")
     public Person1 person1Name() {
         return new Person1("John");
     }
 
-    @GetMapping(path = "/v2/person")
+    //@GetMapping(path = "/person", params = "version=1.1")
+    @GetMapping(path = "/person", headers = "X-API-VERSION=1.1")
     public Person2 person2Name() {
         return new Person2(new Name("Williams", "User 123"));
+    }
+
+
+    @GetMapping(path = "/person", produces = "application/vpn.rakuten.rpay")
+    public Person1 person1NameWithVersion() {
+        return new Person1("Produces version");
     }
 
 
